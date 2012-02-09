4ea392b17becaeb9bc88d4e41b036df487989e0c
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-02-09 21:34:43 +0100

    improved tests

--- test.sh
+++
@@ -1,2 +0,0 @@
-rm /tmp/npr_test/ -rf
-./npr -a examples/Phy0008D92_HUMAN.msf.aa -n examples/Phy0008D92_HUMAN.msf.nt -c ./genetree.cfg -o /tmp/npr_test/ -rename $@
---
+++ test/test.sh
@@ -0,0 +1,2 @@
+rm /tmp/npr_test/ -rf
+../npr -a ../examples/Phy0008D92_HUMAN.msf.aa -n ../examples/Phy0008D92_HUMAN.msf.nt -c ./genetree_test.cfg -o /tmp/npr_test/ $@
