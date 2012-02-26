b4fa3ec73b9c5518d2ebfedd480f45dad5b868f0
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-02-26 14:54:11 +0100

    add fasttree test

---
+++ test/test_plain_fasttree.sh
@@ -0,0 +1,2 @@
+rm /tmp/npr_test_plain_fasttree/ -rf
+../npr -a ../examples/Phy0008D92_HUMAN.msf.aa -n ../examples/Phy0008D92_HUMAN.msf.nt -c ../plain_fasttree.cfg -o /tmp/npr_test_plain_fasttree/ $@
