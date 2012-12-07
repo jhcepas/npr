c8b2ffda220c3cb505ec6bf9ba9ed5b78f71e388
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-12-07 13:56:36 +0100

    close to something...

--- test/genetree/run.sh
+++ test/genetree/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 DN="$(dirname "$(readlink -f "${0}")")"
-find $DN
+find $DN/../../
 $DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --nodetach
