5700f1961049d8f6610a1fb8d1fa080a3cb63990
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-12-11 12:27:37 +0100

    tweaks to add all tool dependencies in portable package

--- test/genetree/run.sh
+++ test/genetree/run.sh
@@ -4,7 +4,7 @@
 # CDE.
 
 DN="$(dirname "$(readlink -f "${0}")")"
-echo "Exploring NPR dir ..."
+echo "Exploring NPR dir ..." $DN/../../
 # This is a trick to ensure all npr files are included in the pkg. CDE
 # will include any file that has been accessed, so I stat every single
 # file
@@ -12,3 +12,6 @@ find $DN/../../ -exec stat {} \; |wc -l
 # Now run a real example, so I can grab all dependencies and put them
 # in the CDE pkg
 $DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --nodetach
+$DN/../../nprdump -a /tmp/tmpresult/
+$DN/../../nprtop /tmp/tmpresult/
+$DN/../../nprete /tmp/tmpresult/final_tree.nw 
