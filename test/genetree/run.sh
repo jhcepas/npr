df8d884479238f78cdcbcee8d043fbd8a262c03e
Jaime Huerta-Cepas <jhcepas@gmail.com>
2012-12-14 15:56:08 +0100

    update test run.sh

--- test/genetree/run.sh
+++ test/genetree/run.sh
@@ -11,7 +11,8 @@ echo "Exploring NPR dir ..." $DN/../../
 find $DN/../../ -exec stat {} \; |wc -l  
 # Now run a real example, so I can grab all dependencies and put them
 # in the CDE pkg
-$DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --nodetach --compress
+$DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --nodetach 
 $DN/../../nprdump -a /tmp/tmpresult/
 $DN/../../nprtop /tmp/tmpresult/
 $DN/../../nprview /tmp/tmpresult/final_tree.nw 
+$DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --compress
