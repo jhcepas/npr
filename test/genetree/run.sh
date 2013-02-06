a0c2a5d7285e55331f33af322f73cfa73370e5c9
Jaime Huerta-Cepas <jhcepas@gmail.com>
2013-02-06 18:08:08 +0100

    unicode string problems...

--- test/genetree/run.sh
+++ test/genetree/run.sh
@@ -8,7 +8,7 @@ echo "Exploring NPR dir ..." $DN/../../
 # This is a trick to ensure all npr files are included in the pkg. CDE
 # will include any file that has been accessed, so I stat every single
 # file
-find $DN/../../ -exec stat {} \; |wc -l  
+find $DN/../../ ! -name '.git/*' -exec stat {} \; |wc -l  
 # Now run a real example, so I can grab all dependencies and put them
 # in the CDE pkg
 $DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1
