29de52353d0135e2971e9eb19cae1e70b0fa5b29
Jaime Huerta-Cepas <jhcepas@gmail.com>
2013-06-21 11:36:58 +0200

    fixes in package building

--- test/genetree/run.sh
+++ test/genetree/run.sh
@@ -8,7 +8,8 @@ echo "Exploring NPR dir ..." $DN/../../
 # This is a trick to ensure all npr files are included in the pkg. CDE
 # will include any file that has been accessed, so I stat every single
 # file
-find $DN/../../ ! -name '.git/*' -exec stat {} \; |wc -l  
+find $DN/../../* -exec stat {} \; |wc -l  
+
 # Now run a real example, so I can grab all dependencies and put them
 # in the CDE pkg
 $DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1
@@ -18,11 +19,14 @@ $DN/../../nprview /tmp/tmpresult/final_tree.nw
 $DN/../../npr -w genetree -c $DN/workflow.cfg -a $DN/Phy0007XAR_HUMAN.msf.aa -o /tmp/tmpresult -x -v4 -t1 --compress
 
 # Ensure this commands are also included
-tar
-gzip
-bzip2
-ls
-cp
-ln 
-du
+for $x in /bin/*; do ldd $cmd; done
+#date
+#tar
+#gzip
+#bzip2
+#ls
+#cp
+#ln 
+#du
+#date
 sqlite3 --version
