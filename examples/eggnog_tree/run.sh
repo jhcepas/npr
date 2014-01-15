05919446fbdb2dacb3b94c65f2ff26586aad3de9
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-01-15 18:07:23 +0100

    added eggnog example

---
+++ examples/eggnog_tree/run.sh
@@ -0,0 +1,1 @@
+../../npr -w genetree -c eggnog_v4_family_tree.cfg -a euNOG09304.fa -o euNOG09304_result/ -x -v2 -t2 --launch_time 2 --clearall --override --compress --logfile --ignore-dup-seqnames $@
