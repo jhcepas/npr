a1bbfe10f6606fa3b6bc6ead7bd4b26e622ab542
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-01-15 15:19:04 +0100

    support for raxml variants and FIXED raxml lk sorting in prottest

--- examples/phylome_tree/run.sh
+++ examples/phylome_tree/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-npr -w genetree -c ../../templates/phylomedb_family_tree.cfg -a Phy0008D92_HUMAN.msf.aa -o result -x -v4 -t1 $@
+npr -w genetree -c ../../templates/phylomedb_family_tree.cfg -a Phy0008D92_HUMAN.msf.aa -o example_results/ -x -v4 -t1 $@
