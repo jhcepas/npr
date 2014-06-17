aa15972a13f418e85b82e8aaf6a3f1244a41b0cf
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-06-17 15:03:23 +0200

    updated examples

--- examples/phylomedb_tree/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh 
-../../npr -w genetree -c phylomedb_family_tree.cfg -a Phy0008D92_HUMAN.msf.aa -o Phy0008D92_results/ -x -v4 -t1 $@
--- examples/supermatrix_tree/run.sh
+++
@@ -1,1 +0,0 @@
-../../npr -o ./result -w supermatrix -a proteome_seqs.fa --cogs cogs.txt --lineages lineages.txt -c fast_npr_sptree_ncbi_targets.cfg -v2 -t2 -x $@
