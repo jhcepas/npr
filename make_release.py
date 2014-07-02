c068e9f31e55f35e90da2505b18e9214879c5f3a
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-07-02 14:46:08 +0000

    release script

--- make_release.py
+++ make_release.py
@@ -3,7 +3,7 @@ a, b, c = map(int, VERSION.split("."))
 c += 1
 NEW_VERSION = "%d.%d.%d" %(a, b, c)
 
-if raw_input("Say [y] to set current commit to new version: %s" NEW_VERSION).strip() == "y":
+if raw_input("Say [y] to set current commit to new version: %s: " %NEW_VERSION).strip() == "y":
     open("VERSION", "w").write(NEW_VERSION)
     git tag VERSION
     git tag "latest_stable"
