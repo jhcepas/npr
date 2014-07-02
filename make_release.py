28c4c7bd96a014e07c7e3f7a3f90ee4a71d77f0b
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-07-02 15:19:21 +0000

    improved basic update script

--- make_release.py
+++ make_release.py
@@ -13,6 +13,7 @@ if raw_input("Say [y] to set current commit to new version: %s: " %NEW_VERSION).
     open("VERSION", "w").write(NEW_VERSION)
     os.system('date "+%F" > DATE')
     os.system("git tag -d latest_stable; git tag -d %s" %(NEW_VERSION))
+    os.system("git push")
     os.system("git tag %s -m '%s' && git tag latest_stable -m '%s' && git push --tags" %(NEW_VERSION, msg, msg))
 
     
