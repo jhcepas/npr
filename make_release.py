295e89040e349abc20cff2c97645b279e2f8bedd
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-07-02 14:50:33 +0000

    release script

--- make_release.py
+++ make_release.py
@@ -1,13 +1,17 @@
+import os
 VERSION = open("VERSION").readline()
 a, b, c = map(int, VERSION.split("."))
-c += 1
+
+if raw_input("Say [y] to increase version number: %s: " %VERSION).strip() == "y":
+    c += 1
+
 NEW_VERSION = "%d.%d.%d" %(a, b, c)
 
 if raw_input("Say [y] to set current commit to new version: %s: " %NEW_VERSION).strip() == "y":
+    msg = raw_input("msg for the release: ")
+    
     open("VERSION", "w").write(NEW_VERSION)
-    git tag VERSION
-    git tag "latest_stable"
-    git push
+    os.system("git tag %s -m '%s' && git tag latest_stable && git push --tags" %(NEW_VERSION, msg))
 
     
 
