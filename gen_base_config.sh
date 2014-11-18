f87e4dbfed781774a4a65fdb06d8a42de05cdeb6
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-11-18 10:47:00 +0000

    added more workflows

--- gen_base_config.sh
+++ gen_base_config.sh
@@ -1,8 +1,8 @@
 python meta_workflow.py -meta  \
-   'name:test_full; 
-    a:muscle_default, mafft_default, clustalo_default, meta_aligner_default; 
-    r:none,trimal01;m:none, prottest_default;  
-    t:fasttree_full,phyml_default,raxml_default'  \
+   'name:test_all; 
+    a:muscle_default,mafft_default,mafft_einsi,mafft_linsi,mafft_ginsi,clustalo_default,meta_aligner_default,meta_aligner_trimmed; 
+    r:none,trimal01,trimal02,trimal_gappyout;m:none, prottest_default;  
+    t:none,fasttree_full,fasttree_default,phyml_default,raxml_default,raxml_default_bootstrap,phyml_default_bootstrap'  \
 \
     'name:aligners_only; 
      a:muscle_default, mafft_default, clustalo_default, meta_aligner_default; 
@@ -24,7 +24,7 @@ python meta_workflow.py -meta  \
      r:none,trimal01;m:none, prottest_default;  
      t:raxml_default'  \
 \
-    'name:test_aligners-phyml; 
+    'name:test_aligners_phyml; 
      a:muscle_default, mafft_default, clustalo_default, meta_aligner_default; 
      r:none,trimal01;m:none;  
      t:phyml_default'  \
@@ -38,6 +38,16 @@ python meta_workflow.py -meta  \
      a:meta_aligner_trimmed; 
      r:trimal01;m:prottest_default;  
      t:phyml_default'  \
+\
+    'name:eggnog41_small; 
+     a:mafft_einsi; 
+     r:trimal01;m:prottest_default;  
+     t:phyml_default'  \
+\
+    'name:eggnog41_large; 
+     a:clustalo_default; 
+     r:trimal01;m:none;  
+     t:fasttree_full'  \
 \
     'name:standard_fasttree; 
      a:clustalo_default; 
